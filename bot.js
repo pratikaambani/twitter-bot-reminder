c6770a5b29e797fd44dfe561d4ea7ff8b065574d
Pratik Ambani <pratikaambani@gmail.com>
2018-04-19 23:47:50 +0530

    won't disturb me in the midnight

--- bot.js
+++ bot.js
@@ -6,16 +6,18 @@ var
 
 var Twitter = new twit(config);
 
-// Water Reminder Bot, will remind me to drink water every hour
+// Reminder Bot, will remind me to drink water every hour
 var tweet = function() {
     var date = moment().tz("Asia/Kolkata");
     console.log(date);
 
-    Twitter.post('statuses/update', {status: date+' | Hey Buddy!!\nTime to drink Water!!'}, function(error, tweet) {
-        if (!error) {
-            console.log(tweet);
-        }
-    });
+    if(6<parseInt(date.format('h'))) {
+        Twitter.post('statuses/update', {status: date+' | Hey Buddy!!\nTime to drink Water!!'}, function(error, tweet) {
+            if (!error) {
+                console.log(tweet);
+            }
+        });
+    }
 }
 
 tweet();
